e9cb0457ccffb452331d2e8a4e6231eff4bdabec
DanielCH5 <163397248+DanielCH5@users.noreply.github.com>
2025-05-22 00:39:34 +0200

    Trying out a different approach with creating them a column at a time

--- script.js
+++ script.js
@@ -3,18 +3,35 @@
 
 const container = document.querySelector('.container');
 //Create an array for the divs in the grid
-const gridItems = [];
+// const gridItems = [];
 //Create a loop that creates 256 divs and pushes them
 //into the gridItems array
-for (i = 0; i < 256; i++){
-    const gridItem = document.createElement('div');
-    gridItem.style.backgroundColor = 'red';
-    gridItem.style.display = 'flex';
-    gridItems.push(gridItem);
-};
+// for (i = 0; i < 256; i++){
+//     const gridItem = document.createElement('div');
+//     gridItem.style.backgroundColor = 'red';
+//     gridItems.push(gridItem);
+// };
 //Appends all of the items to the container
-for (let item of gridItems){
-    container.appendChild(item);
+// for (let item of gridItems){
+//     container.appendChild(item);
+// };
+
+
+//We have to change it to create 16 cells in a row
+//and then create that row 16 times
+
+function makeGrid(num){
+    const row = [];
+    for (i = 0; i < num; i++){
+        const cell = document.createElement('div');
+        cell.style.backgroundColor = 'green';
+        row.push(cell);
+    }
+    
+    for (let cell of row){
+        container.appendChild(cell);
+    }
+    
 };
 
-
+makeGrid(16);
